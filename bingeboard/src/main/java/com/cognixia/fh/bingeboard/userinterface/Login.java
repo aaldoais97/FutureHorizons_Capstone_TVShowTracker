ada734e65596fa109ff0fc3a657a1af9dca96dd4
Aimen Al-Doais <aaldoais97@gmail.com>
2025-06-25 11:36:15 -0400

    Added placeholder for existing user credential check in userinterface > Login

--- bingeboard/src/main/java/com/cognixia/fh/bingeboard/userinterface/Login.java
+++ bingeboard/src/main/java/com/cognixia/fh/bingeboard/userinterface/Login.java
@@ -23,6 +23,9 @@ public class Login {
             System.out.println("Please enter your password and press 'enter':");
             password = loginScanner.nextLine();
 
+            // Check if login combination matches existing user
+            // INSERT CODE HERE TO CHECK SQL DB
+
             // Validate password with a length and regex to check for at least 6 characters, 1 digit, and 1 symbol
             if (password.length() > 6 && password.matches("(?=.*\\d)(?=.*[^a-zA-Z0-9]).+")) {
                 validLogin = true; // Valid login, exit loop
