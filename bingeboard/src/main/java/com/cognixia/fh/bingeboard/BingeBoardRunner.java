e026fe189a4957717905c9e60694fb19c6057efc
Aimen Al-Doais <aaldoais97@gmail.com>
2025-06-25 11:33:51 -0400

    Created Java project and partially implemented login page

---
+++ bingeboard/src/main/java/com/cognixia/fh/bingeboard/BingeBoardRunner.java
@@ -0,0 +1,15 @@
+package com.cognixia.fh.bingeboard;
+
+import com.cognixia.fh.bingeboard.userinterface.Login;
+
+/**
+ * Hello world!
+ *
+ */
+public class BingeBoardRunner 
+{
+    public static void main( String[] args )
+    {
+        Login.homePage(); // Start the login process
+    }
+}
---
+++ bingeboard/src/main/java/com/cognixia/fh/bingeboard/userinterface/Login.java
@@ -0,0 +1,35 @@
+package com.cognixia.fh.bingeboard.userinterface;
+
+import java.util.Scanner;
+
+public class Login {
+    private static String username;
+    private static String password;
+
+    public static void homePage() {
+        Scanner loginScanner = new Scanner(System.in);
+        boolean validLogin = false;
+
+        System.out.println("Welcome to the BingeBoard!");
+        System.out.println("Your entertainment tracking hub for all things binge-worthy!");
+        System.out.println("Please log in or sign up by entering a username and password.");
+        
+        while (!validLogin) {
+            // Get username
+            System.out.println("Please enter your username and press 'enter':");
+            username = loginScanner.nextLine(); // No need for try-catch here
+
+            // Get password
+            System.out.println("Please enter your password and press 'enter':");
+            password = loginScanner.nextLine();
+
+            // Validate password with a length and regex to check for at least 6 characters, 1 digit, and 1 symbol
+            if (password.length() > 6 && password.matches("(?=.*\\d)(?=.*[^a-zA-Z0-9]).+")) {
+                validLogin = true; // Valid login, exit loop
+                System.out.println("Login successful! Welcome, " + username + "!");
+            } else {
+                System.out.println("Password must be at least 6 characters long and contain at least 1 digit and 1 symbol.");
+            }
+        }
+    }
+}
